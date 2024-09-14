44a90e23378bbc8c90b28f31b4d99d97a273d681
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-09-14 22:15:34 +0100

    script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -9,8 +9,8 @@ async function main() {
   });
 
   // initialize existing predeployed account 0
-  console.log("ACCOUNT_ADDRESS=", process.env.DEPLOYER_ADDRESS);
-  console.log("ACCOUNT_PRIVATE_KEY=", process.env.DEPLOYER_PRIVATE_KEY);
+  //console.log("ACCOUNT_ADDRESS=", process.env.DEPLOYER_ADDRESS);
+  //console.log("ACCOUNT_PRIVATE_KEY=", process.env.DEPLOYER_PRIVATE_KEY);
   const privateKey0 = process.env.DEPLOYER_PRIVATE_KEY ?? "";
   const accountAddress0: string = process.env.DEPLOYER_ADDRESS ?? "";
   const account0 = new Account(provider, accountAddress0, privateKey0);
@@ -31,8 +31,7 @@ async function main() {
   const myCallData = new CallData(sierraCode.abi);
   const constructor = myCallData.compile("constructor", {
     initial_value: 100,
-    kill_switch:
-      "0x05f7151ea24624e12dde7e1307f9048073196644aa54d74a9c579a257214b542",
+    kill_switch_address:"0x05f7151ea24624e12dde7e1307f9048073196644aa54d74a9c579a257214b542",
     initial_owner: process.env.DEPLOYER_ADDRESS ?? "",
   });
   const deployResponse = await account0.declareAndDeploy({
